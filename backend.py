552572f47df6a0b737b63fd0780e32df15351130
= <adahm7114@gmail.com>
2025-08-02 04:07:49 +0100

    initial commit

---
+++ backend.py
@@ -0,0 +1,34 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import AIMessage, HumanMessage, BaseMessage
+from dotenv import load_dotenv
+from typing import Annotated
+from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import InMemorySaver
+import time
+import os
+
+
+
+load_dotenv()
+llm = ChatOpenAI()
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+    
+    
+def chatbot(ChatState) -> dict:
+    message = llm.invoke(ChatState['messages']).content
+    return {"messages":AIMessage(message)}
+
+
+checkpointer = InMemorySaver()
+graph = StateGraph(ChatState)
+
+graph.add_node("chatbot", chatbot)
+
+graph.add_edge(START, "chatbot")
+graph.add_edge("chatbot", END)
+
+workflow = graph.compile(checkpointer=checkpointer)
---
+++ frontend.py
@@ -0,0 +1,71 @@
+import streamlit as st
+import time
+import streamlit.components.v1 as components
+from langchain_core.messages import AIMessage, HumanMessage, BaseMessage
+from backend import workflow
+import os
+
+if 'output' not in st.session_state:
+    res = st.chat_input('write your id...', )
+    
+    if res in ['1', '2', '3']:    
+    
+        st.session_state['output'] = res
+        st.rerun()
+   
+    elif res != None:
+        st.title("")
+        st.error("🚨 Unauthorized access detected!")
+        time.sleep(1.5)
+
+        st.error("📡 Tracing IP address...")
+        time.sleep(1.7)
+        
+        st.error("📡 IP address: 192.168.43.127")
+        time.sleep(1.7)
+        
+        os.system("afplay screen-capture-sound-on-phone-or-pc.mp3")  # Replace with your file name
+        st.error("💾 Screenshot taken and archived.")
+        time.sleep(1.3)
+
+        st.error("💀 You have 30 seconds to disconnect.")
+        time.sleep(1.3)
+        
+        st.error("📞 Calling 911 and reporting digital break-in.")
+        os.system("afplay car-siren-police-ambulance-fire.mp3 &")  
+    else:
+        st.title("State your Id ...")
+    
+else:
+    
+    st.title("🧠 Chatbot")
+    if 'messages' in st.session_state:
+        pass
+    else:
+        st.session_state['messages'] = []
+
+    for role_content in st.session_state['messages']:
+        with st.chat_message(role_content['role']):
+            st.text(role_content['content'])
+
+        
+    response = st.chat_input("write something here:..", )
+
+    if response:
+        st.session_state['messages'].append({'role': 'user', 'content': HumanMessage(response).content})
+
+        with st.chat_message('user'):
+            st.text(response)    
+            
+        with st.chat_message('assistant'):
+            thread_id = '1'
+            config = {"configurable": {"thread_id": thread_id}}
+            
+            ai_response = workflow.invoke({"messages": HumanMessage(response)}, config=config)['messages'][-1].content
+            
+            st.session_state['messages'].append({'role': 'assistant', 'content': ai_response})
+            st.text(ai_response)
+                
+    
+ 
+                
